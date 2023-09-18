3224a156854701d29fc269f62ad5e721fa0e611c
Jono Lane <lane7520@gmail.com>
2023-09-18 14:27:53 -0500

    messaging around with different templates

--- jono-lane/src/App.js
+++ jono-lane/src/App.js
@@ -1,25 +1,283 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* navbar */}
+      <nav className="navbar navbar-expand-lg fixed-top navbar-dark navbarScroll">
+        <div className="container">
+          <a className="navbar-brand" href="#">Jono</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#about">About</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#services">Services</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#portfolio">Portfolio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* main banner */}
+      <section className="bgimage" id="home">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 hero-text">
+              <h2 className="hero_title">Hi, it's me Jono</h2>
+              <p className="hero_desc">I am a professional freelancer in Nashville</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* about section*/}
+      <section id="about">
+        <div className="container mt-4 pt-4">
+          <h1 className="text-center">About Me</h1>
+          <div className="row mt-4">
+            <div className="col-lg-4">
+              <img src="images/headshot.jpg" className="imageAboutPage" alt />
+            </div>
+            <div className="col-lg-8">
+              <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged
+              </p>
+              <div className="row mt-3">
+                <div className="col-md-6">
+                  <ul>
+                    <li>Name: David Parker</li>
+                    <li>Age: 28</li>
+                    <li>Occupation: Web Developer</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <p> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div></section>
+      {/* services section*/}
+      <section id="services">
+        <div className="container">
+          <h1 className="text-center">Services</h1>
+          <div className="row">
+            <div className="col-lg-4 mt-4">
+              <div className="card servicesText">
+                <div className="card-body">
+                  <i className="fas servicesIcon fa-clock" />
+                  <h4 className="card-title mt-3">Website Development</h4>
+                  <p className="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card servicesText">
+                <div className="card-body">
+                  <i className="fas servicesIcon fa-layer-group" />
+                  <h4 className="card-title mt-3">Website Design</h4>
+                  <p className="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card servicesText">
+                <div className="card-body">
+                  <i className="far servicesIcon fa-check-circle" />
+                  <h4 className="card-title mt-3">Website Deployment</h4>
+                  <p className="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 mt-4">
+              <div className="card servicesText">
+                <div className="card-body">
+                  <i className="fas servicesIcon fa-search" />
+                  <h4 className="card-title mt-3">SEO</h4>
+                  <p className="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card servicesText">
+                <div className="card-body">
+                  <i className="fas servicesIcon fa-shield-alt" />
+                  <h4 className="card-title mt-3">DevOps</h4>
+                  <p className="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card servicesText">
+                <div className="card-body">
+                  <i className="fas servicesIcon fa-wrench" />
+                  <h4 className="card-title mt-3">QA</h4>
+                  <p className="card-text mt-3">Some quick example text to build on the card title and make up the bulk of the card's content.
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* portfolio section*/}
+      <section id="portfolio">
+        <div className="container mt-3">
+          <h1 className="text-center">Portfolio</h1>
+          <div className="row">
+            <div className="col-lg-4 mt-4">
+              <div className="card">
+                <img className="card-img-top" src="images/portfolioImage1.jpg" alt="Card image" style={{ width: '100%' }} />
+                <div className="card-body">
+                  <h4 className="card-title">YouTube Clone</h4>
+                  <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-success">Link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card portfolioContent">
+                <img className="card-img-top" src="images/portfolioImage4.jpg" alt="Card image" style={{ width: '100%' }} />
+                <div className="card-body">
+                  <h4 className="card-title">Quiz App</h4>
+                  <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-success">Link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card portfolioContent">
+                <img className="card-img-top" src="images/portfolioImage3.jpg" alt="Card image" style={{ width: '100%' }} />
+                <div className="card-body">
+                  <h4 className="card-title">Product Landing Page</h4>
+                  <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-success">Link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br />
+          <div className="row">
+            <div className="col-lg-4 mt-4">
+              <div className="card portfolioContent">
+                <img className="card-img-top" src="images/portfolioImage4.jpg" alt="Card image" style={{ width: '100%' }} />
+                <div className="card-body">
+                  <h4 className="card-title">Messaging Service</h4>
+                  <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-success">Link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card portfolioContent">
+                <img className="card-img-top" src="images/portfolioImage1.jpg" alt="Card image" style={{ width: '100%' }} />
+                <div className="card-body">
+                  <h4 className="card-title">Twitter Clone</h4>
+                  <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-success">Link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 mt-4">
+              <div className="card portfolioContent">
+                <img className="card-img-top" src="images/portfolioImage4.jpg" alt="Card image" style={{ width: '100%' }} />
+                <div className="card-body">
+                  <h4 className="card-title">Blog App</h4>
+                  <p className="card-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                  <div className="text-center">
+                    <a href="#" className="btn btn-success">Link</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div></section>
+      {/* contact section*/}
+      <section id="contact">
+        <div className="container mt-3 contactContent">
+          <h1 className="text-center">Contact Me</h1>
+          <div className="row mt-4">
+            <div className="col-lg-6">
+              {/* to edit google map goto https://www.embed-map.com type your location, generate html code and copy the html  */}
+              <div style={{ maxWidth: '100%', overflow: 'hidden', color: 'red', width: 500, height: 500 }}>
+                <div id="embedmap-canvas" style={{ height: '100%', width: '100%', maxWidth: '100%' }}>
+                  <iframe style={{ height: '100%', width: '100%', border: 0 }} frameBorder={0} src="https://www.google.com/maps/embed/v1/place?q=new+york&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8">
+                  </iframe>
+                </div>
+                <a className="googlemaps-html" href="https://www.embed-map.com" id="get-data-forembedmap">https://www.embed-map.com</a>
+                <style dangerouslySetInnerHTML={{ __html: "#embedmap-canvas img{max-width:none!important;background:none!important;font-size: inherit;font-weight:inherit;}\n                        " }} />
+              </div>
+            </div>
+            <div className="col-lg-6">
+              {/* form fields */}
+              <form>
+                <input type="text" className="form-control form-control-lg" placeholder="Name" />
+                <input type="email" className="form-control mt-3" placeholder="Email" />
+                <input type="text" className="form-control mt-3" placeholder="Subject" />
+                <div className="mb-3 mt-3">
+                  <textarea className="form-control" rows={5} id="comment" name="text" placeholder="Project Details" defaultValue={""} />
+                </div>
+              </form>
+              <button type="button" className="btn btn-success mt-3">Contact Me</button>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* footer section*/}
+      <footer id="footer">
+        <div className="container-fluid">
+          {/* social media icons */}
+          <div className="social-icons mt-4">
+            <a href="https://www.facebook.com/" target="_blank"><i className="fab fa-facebook" /></a>
+            <a href="https://www.instagram.com/" target="_blank"><i className="fab fa-instagram" /></a>
+            <a href="https://www.twitter.com/" target="_blank"><i className="fab fa-twitter" /></a>
+            <a href="https://www.linkedin.com/" target="_blank"><i className="fab fa-linkedin" /></a>
+            <a href="https://www.twitch.tv/" target="_blank"><i className="fab fa-twitch" /></a>
+          </div>
+        </div>
+      </footer>
     </div>
+
   );
-}
+};
 
 export default App;

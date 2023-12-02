fffe02df79ea6a0a57cee5948de51321dbb23b70
Jono Lane <lane7520@gmail.com>
2023-12-02 17:46:23 -0600

    path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,10 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  pathPrefix: '/public',
+  pathPrefix: '',
   siteMetadata: {
     title: `jono-lane`,
-    siteUrl: `https://www.jonolane.github.io/public`,
+    siteUrl: `https://www.jonolane.github.io/public`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin

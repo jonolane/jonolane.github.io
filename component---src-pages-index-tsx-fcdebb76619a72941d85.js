e189a03875c40a5a5fb36b69faa20817a5b575fb
Jono Lane <lane7520@gmail.com>
2023-12-03 13:20:08 -0600

    Updates

---
+++ component---src-pages-index-tsx-fcdebb76619a72941d85.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjono_lane=self.webpackChunkjono_lane||[]).push([[691],{7200:function(e,t,n){n.r(t),n.d(t,{Head:function(){return c}});var a=n(7294);t.default=()=>a.createElement("main",{className:"flex items-center justify-center h-screen bg-black"},a.createElement("h1",{className:"text-white text-center text-4xl"},"Jono Lane"));const c=()=>a.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-fcdebb76619a72941d85.js.map

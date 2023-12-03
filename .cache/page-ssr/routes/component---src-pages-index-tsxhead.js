36688d2b8f1a6d3b12d558312498eb9f13582d90
Jono Lane <lane7520@gmail.com>
2023-12-03 13:55:50 -0600

    dont want the cache to get too full

---
+++ .cache/page-ssr/routes/component---src-pages-index-tsxhead.js
@@ -0,0 +1,146 @@
+"use strict";
+exports.id = "component---src-pages-index-tsxhead";
+exports.ids = ["component---src-pages-index-tsxhead"];
+exports.modules = {
+
+/***/ "./src/pages/index.tsx?export=head":
+/*!*****************************************!*\
+  !*** ./src/pages/index.tsx?export=head ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Head: () => (/* binding */ Head),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif"
+};
+const headingStyles = {
+  marginTop: 0,
+  marginBottom: 64,
+  maxWidth: 320
+};
+const headingAccentStyles = {
+  color: "#663399"
+};
+const paragraphStyles = {
+  marginBottom: 48
+};
+const codeStyles = {
+  color: "#8A6534",
+  padding: 4,
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
+  borderRadius: 4
+};
+const listStyles = {
+  marginBottom: 96,
+  paddingLeft: 0
+};
+const doclistStyles = {
+  paddingLeft: 0
+};
+const listItemStyles = {
+  fontWeight: 300,
+  fontSize: 24,
+  maxWidth: 560,
+  marginBottom: 30
+};
+const linkStyle = {
+  color: "#8954A8",
+  fontWeight: "bold",
+  fontSize: 16,
+  verticalAlign: "5%"
+};
+const docLinkStyle = {
+  ...linkStyle,
+  listStyleType: "none",
+  display: `inline-block`,
+  marginBottom: 24,
+  marginRight: 12
+};
+const descriptionStyle = {
+  color: "#232129",
+  fontSize: 14,
+  marginTop: 10,
+  marginBottom: 0,
+  lineHeight: 1.25
+};
+const docLinks = [{
+  text: "TypeScript Documentation",
+  url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
+  color: "#8954A8"
+}, {
+  text: "GraphQL Typegen Documentation",
+  url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
+  color: "#8954A8"
+}];
+const badgeStyle = {
+  color: "#fff",
+  backgroundColor: "#088413",
+  border: "1px solid #088413",
+  fontSize: 11,
+  fontWeight: "bold",
+  letterSpacing: 1,
+  borderRadius: 4,
+  padding: "4px 6px",
+  display: "inline-block",
+  position: "relative",
+  top: -2,
+  marginLeft: 10,
+  lineHeight: 1
+};
+const links = [{
+  text: "Tutorial",
+  url: "https://www.gatsbyjs.com/docs/tutorial/getting-started/",
+  description: "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
+  color: "#E95800"
+}, {
+  text: "How to Guides",
+  url: "https://www.gatsbyjs.com/docs/how-to/",
+  description: "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
+  color: "#1099A8"
+}, {
+  text: "Reference Guides",
+  url: "https://www.gatsbyjs.com/docs/reference/",
+  description: "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
+  color: "#BC027F"
+}, {
+  text: "Conceptual Guides",
+  url: "https://www.gatsbyjs.com/docs/conceptual/",
+  description: "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+  color: "#0D96F2"
+}, {
+  text: "Plugin Library",
+  url: "https://www.gatsbyjs.com/plugins",
+  description: "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+  color: "#8EB814"
+}, {
+  text: "Build and Host",
+  url: "https://www.gatsbyjs.com/cloud",
+  badge: true,
+  description: "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+  color: "#663399"
+}];
+const IndexPage = () => {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("main", {
+    className: "flex items-center justify-center h-screen bg-black"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("h1", {
+    className: "text-white text-center text-4xl"
+  }, "Jono Lane"));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (IndexPage);
+const Head = () => /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0__.createElement("title", null, "Home Page");
+
+/***/ })
+
+};
+;
+//# sourceMappingURL=component---src-pages-index-tsxhead.js.map

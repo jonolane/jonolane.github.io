8703be9797f39c10d8a52c2cbd7ab66eaf93922e
Jono Lane <lane7520@gmail.com>
2023-12-07 20:40:15 -0600

    using github token

--- src/controllers/githubApi.ts
+++ src/controllers/githubApi.ts
@@ -1,7 +1,7 @@
 export async function fetchRepositories() {  
     const repoResponse = await fetch("https://api.github.com/user/repos", {
       headers: {
-        Authorization: `Bearer ${process.env.JONO_TOKEN}`,
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
       },
     });
   
@@ -12,5 +12,3 @@ export async function fetchRepositories() {
     const repositories = await repoResponse.json();
     return repositories;
   }
-
-  // action config change

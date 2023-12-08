8d3efa5ad9eb5b0e5bfe843d20297acf763705c9
Jono Lane <lane7520@gmail.com>
2023-12-08 17:35:29 -0600

    more octokit

--- src/controllers/githubApi.ts
+++ src/controllers/githubApi.ts
@@ -1,7 +1,10 @@
 import { Octokit } from "@octokit/rest";
 
 export async function fetchRepositories() {
-  const octokit = new Octokit({ });
+  const octokit = new Octokit({ 
+    auth: 'proceess.env.TOKEN'
+  });
+  
 
   try {
     const response = await octokit.request("GET /user/repos", {

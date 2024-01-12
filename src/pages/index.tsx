ed9215e14faed0905c9f12b9e77d4f5bf4b26824
jonolane <lane7520@gmail.com>
2024-01-11 20:07:47 -0600

    adjust file path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -113,7 +113,7 @@ const Head: React.FC<HeadProps> = () => {
       {/* linkedin og: tags for preview */}
       <meta name="title" property="og:title" content="Jono Lane" />
       <meta name="description" property="og:description" content="I'm Jono Lane, a full-stack developer based in Nashville. I enjoy creating impactful products that adapt to the ever-changing landscape of technology." />
-      <meta name="image" property="og:image" content="https://jonolane.io/preview.jpg" />
+      <meta name="image" property="og:image" content="https://www.jonolane.io/preview.jpg" />
       <meta name="description" property="og:url" content="https://jonolane.io" />
 
       <title>Jono Lane</title>

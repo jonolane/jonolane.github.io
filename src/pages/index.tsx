7d51054f75de4996e3bc036b1c227b46c3303dcd
jonolane <lane7520@gmail.com>
2024-01-12 12:01:28 -0600

    head api ordering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -112,9 +112,9 @@ export const Head: React.FC<HeadProps> = () => {
   return (
     <>
       {/* linkedin og: tags for preview */}
+      <meta name="image" property="og:image" content="https://jonolane.io/preview2.jpg" />
       <meta name="title" property="og:title" content="Jono Lane" />
       <meta name="description" property="og:description" content="I'm Jono Lane, a full-stack developer based in Nashville. I enjoy creating impactful products that adapt to the ever-changing landscape of technology." />
-      <meta name="image" property="og:image" content="https://jonolane.io/preview2.jpg" />
       {/* <meta name="url" property="og:url" content="https://jonolane.io" /> */}
 
       <title>Jono Lane</title>
